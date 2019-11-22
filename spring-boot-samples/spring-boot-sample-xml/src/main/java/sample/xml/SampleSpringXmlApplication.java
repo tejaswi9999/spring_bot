4a2149f73ac9afeef109364eb77eccc96b461f4f
teju <tejaswirayankula@gmail.com>
2019-11-21 19:11:19 -0500

    added new line in java file it's just a commet

--- spring-boot-samples/spring-boot-sample-xml/src/main/java/sample/xml/SampleSpringXmlApplication.java
+++ spring-boot-samples/spring-boot-sample-xml/src/main/java/sample/xml/SampleSpringXmlApplication.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+//this is just a added comment line
 package sample.xml;
 
 import sample.xml.service.HelloWorldService;
